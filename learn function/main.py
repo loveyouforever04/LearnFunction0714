4e2b62a8be0c491ac0002ba6f8e6a4955db92fed
loveyouforever04 <1691049547@qqcom>
2019-07-14 17:58:01 +0800

    0714xfy1

---
+++ learn function/main.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+from my_modules import*
+#add函数
+prt_cut_line(1)
+
+x = 1
+y = 2
+c = x+y
+print(c)
+
+c= add(x,y)
+print(c)
+#---长方体----
+prt_cut_line(2)
+s=cal_rect_area(10,100)
+print(s)
---
+++ learn function/my_modules.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+
+def  add(a,b):
+    '这是一个函数 add'
+    sum = a + b
+    return sum
+
+
+def cal_rect_area(lenth,width):
+    '计算长方体面积'
+    area=lenth*width
+    return area
+
+def prt_cut_line(sn):
+    '打印一条华丽分割线'
+    print('\n-----cutting line {%d}-----'%sn)
+    return
